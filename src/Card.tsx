7e10a4cd508e6dac7dc09b94b40e6125611f410c
Simon Kendall <simon.a.kendall@gmail.com>
2024-06-14 11:27:28 +0200

    make cards move when you click them

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
+type CardVector = {
+  gridRowStart: number;
+  gridColumnStart: number;
+};
+
 const Card = () => {
+  const [cardVector, setCardVector]: [
+    cardVector: CardVector,
+    setCardVector: (cv: CardVector) => void
+  ] = useState({ gridRowStart: 0, gridColumnStart: 1 });
+
+  const [gridRow, setGridRow] = useState(0);
+  const [gridColumn, setGridColumn] = useState(1);
+
+  const moveRight = () => {
+    /*console.log("WHAT");
+    const cv: CardVector = cardVector;
+    console.log(cv);
+    cv.gridColumnStart++;
+    console.log(cv);
+    setCardVector(cv);*/
+    console.log("yo");
+    setGridColumn(gridColumn + 1);
+  };
+
   return (
     <>
-      <textarea className="grid-item">Default value</textarea>
+      <textarea
+        onClick={moveRight}
+        //style={{ ...cardVector }}
+        style={{ gridRowStart: gridRow, gridColumnStart: gridColumn }}
+        className="grid-item"
+        defaultValue="yo what up"
+      ></textarea>
     </>
   );
 };
