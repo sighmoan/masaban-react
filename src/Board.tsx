dd8e0dca8125a8ddff73e4f0b968aac675bc82c3
Simon Kendall <simon.a.kendall@gmail.com>
2024-06-27 12:13:49 +0200

    add button to add columns

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react";
 import Column from "./Column.tsx";
 import { LiftedCardContext } from "./LiftedCardContext.tsx";
 import { LiftedCardState, ColumnTransfer } from "./types.ts";
-import { apiGetColumns } from "./_apiService.ts";
-import { useQuery } from "@tanstack/react-query";
+import { apiGetColumns, apiAddColumn } from "./_apiService.ts";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const Board = ({ boardId }: { boardId: string }) => {
   const [liftedCard, setLiftedCard] = useState(null);
+  const queryClient = useQueryClient();
 
   const baseApiUrl = "nothing/";
 
@@ -20,8 +21,22 @@ const Board = ({ boardId }: { boardId: string }) => {
     setLiftedCard: setLiftedCard,
   };
 
+  const addColumnMutation = useMutation({
+    mutationFn: () => {
+      return apiAddColumn("", boardId, "New column", 2);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [boardId] });
+    },
+  });
+
+  const addColumn = () => {
+    addColumnMutation.mutate();
+  };
+
   return (
     <>
+      <button onClick={addColumn}>Add Column</button>
       <div className="grid-container" id="grid-container">
         <LiftedCardContext.Provider value={cardObj}>
           {columns.data?.map((col) => (
